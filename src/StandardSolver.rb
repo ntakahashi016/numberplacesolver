7f8f28060fd624980dce5bada1277f5cf48a98ac
Noriaki Takahashi <ntakahashi016@gmail.com>
2020-01-09 16:14:26 +0900

    Hidden PairをHiddenSubsetに拡張

--- src/StandardSolver.rb
+++ src/StandardSolver.rb
@@ -45,6 +45,14 @@ class StandardSolver < Solver
       when :hidden_pair
         # Hidden pair 同じ領域で2種類の数字が配置できるマスが同じ2マスに限られる場合、その領域から他の候補を排除する
         changed = remove_hidden_subsets(2)
+        state = changed ? :init : :hidden_triple
+      when :hidden_triple
+        # Hidden triple 同じ領域で3種類の数字が配置できるマスが同じ3マスに限られる場合、その領域から他の候補を排除する
+        changed = remove_hidden_subsets(3)
+        state = changed ? :init : :hidden_quadruple
+      when :hidden_quadruple
+        # Hidden triple 同じ領域で3種類の数字が配置できるマスが同じ3マスに限られる場合、その領域から他の候補を排除する
+        changed = remove_hidden_subsets(4)
         state = changed ? :init : :fail
       when :fail
         raise "問題を解けませんでした"
@@ -398,43 +406,59 @@ class StandardSolver < Solver
       candidates.each do |candidate|
         cells_by_candidate[candidate] = (cells.select{|cell| cell.candidates.include?(candidate) })
       end
-      common_cells = {} # 候補のペアをキーとしそれらの候補が入りうるマスの配列を値とするハッシュ
-      for i in 2..n do
-        cells_by_candidate.keys.combination(i).each do |pair|
-          cc = nil
-          pair.each do |p|
-            unless cc
-              cc = cells_by_candidate[p]
-              next
-            end
-            cc &= cells_by_candidate[p]
-          end
-          common_cells[pair] = cc
+
+      # ある候補のは入りうるマスの集合がもう一つの候補の入りうるマスの集合と包含関係にあるか調べる
+      # A=BのときはAがBを包含するという関係のみを保存する
+      including_relation = cells_by_candidate.keys.combination(2).collect do |a,b|
+        if (cells_by_candidate[a] | cells_by_candidate[b]) - cells_by_candidate[a] == []
+          [a,b]
+        elsif (cells_by_candidate[b] | cells_by_candidate[a]) - cells_by_candidate[b] == []
+          [b,a]
+        else
+          nil
         end
       end
-      common_cells.each_key do |key_candidates|
-        # 候補のペアを配置できるマスとそれぞれの候補が配置できるマスが一致するかどうかをチェック
-        check = key_candidates.all? do |key_candidate|
-          if common_cells[key_candidates] == [] || cells_by_candidate[key_candidate] == []
-            false
-          else
-            common_cells[key_candidates] == cells_by_candidate[key_candidate] && common_cells[key_candidates].length == n
-          end
+
+      including_relation.compact!
+
+      including_relation_hash = {}
+      including_relation.each do |a,b|
+        including_relation_hash[a] = [] unless including_relation_hash[a]
+        including_relation_hash[a] << b
+      end
+
+      hidden_subset = {}
+      including_relation_hash.each do |a,bs|
+        tmp_subset = cells_by_candidate[a]
+        bs.each do |b|
+          tmp_subset &= cells_by_candidate[a] - cells_by_candidate[b]
         end
-        if check
-          common_cells[key_candidates].each do |cell|
-            prev_candidates = cell.candidates
-            target = cell.candidates - key_candidates
-            tmp = cell.candidates
-            next if target == []
-            after_candidates = cell.delete_candidates(target)
-            puts "#####{__method__}(#{n}):#{cell.x.to_s},#{cell.y.to_s} #{prev_candidates} => #{after_candidates}"
-            result = true if prev_candidates != after_candidates
-          end
+        next if tmp_subset != []
+        hidden_subset[a] = [] unless hidden_subset[a]
+        hidden_subset[a] << a
+        bs.each { |b| hidden_subset[a] << b }
+      end
+
+      exclusive = []
+      hidden_subset.each_key do |a|
+        exclusive << a if cells_by_candidate[a].size != n || hidden_subset[a].size != n
+      end
+      exclusive.each do |e|
+        hidden_subset.delete(e)
+      end
+
+      hidden_subset.each_key do |a|
+        cells_by_candidate[a].each do |cell|
+          prev_candidates = cell.candidates
+          target = cell.candidates - hidden_subset[a]
+          tmp = cell.candidates
+          next if target == []
+          after_candidates = cell.delete_candidates(target)
+          puts "#####{__method__}(#{n}):#{cell.x.to_s},#{cell.y.to_s} #{prev_candidates} => #{after_candidates}"
+          result = true if prev_candidates != after_candidates
         end
       end
     end
     result
   end
 end
-
