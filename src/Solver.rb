4577a1b15216496803cc90d5c1372b0709a02e8d
Noriaki Takahashi <ntakahashi016@gmail.com>
2020-01-22 16:32:10 +0900

    Solverを廃しStandardSolverをSolverとする

--- src/Solver.rb
+++ src/Solver.rb
@@ -1,14 +1,36 @@
 # coding: utf-8
+
+require './Strategy'
+
 class Solver
 
-  def set_board(board)
-    raise TypeError,"Class:#{self.class.name}:初期化に失敗しました" unless board.class == Board
-    @board = board
+  def initialize
+    @strategies = {}
   end
 
   def solve()
+    i = 0
+    until @board.solved?
+      raise "問題を解けませんでした" if i >= @strategies.size
+      names = @strategies.keys
+      changed = @strategies[names[i]].solve(@board)
+      if changed
+        i = 0
+      else
+        i += 1
+      end
+    end
   end
 
-  def add_strategy(strategy)
+  def add_strategy(name,strategy)
+    raise TypeError unless String === name
+    raise TypeError unless Strategy === strategy
+    @strategies[name] = strategy
+  end
+
+  def del_strategy(name,strategy)
+    raise TypeError unless String === name
+    raise TypeError unless Strategy === strategy
+    @strategies.delete(name)
   end
 end
